c2dace20bf4e2861ef213cabb620220c44f22673
Rasmus Eskola <fruitiex@gmail.com>
2014-03-11 06:46:12 +0200

    set correct device for partition in new setup, sleep before usb passthrough

--- usb-passthrough-add.sh
+++ usb-passthrough-add.sh
@@ -2,12 +2,18 @@
 
 # wait for qemu to start listening on the unix socket
 sleep 1
+echo "
+{ \"execute\": \"qmp_capabilities\" }
+" | nc -U /home/rasse/vm/qmp-sock
+
+# let windows boot
+sleep 60
 
 SEARCH=(
 "Logitech, Inc. Unifying Receiver"
 "Ideazon, Inc."
-"Microsoft Corp. Xbox360 Controller"
 "Holtek Semiconductor, Inc."
+"Microsoft Corp. Xbox360 Controller"
 )
 USB_DEVICES=()
 
@@ -18,10 +24,12 @@ done
 for i in "${USB_DEVICES[@]}"; do
 vendor=$((0x$(echo $i | cut -d: -f1)))
 product=$((0x$(echo $i | cut -d: -f2)))
+echo "Passing through (USB) $i..."
 echo "
 { \"execute\": \"qmp_capabilities\" }
 { \"execute\": \"device_add\", \"arguments\": { \"driver\": \"usb-host\", \"vendorid\": \"$vendor\", \"productid\": \"$product\" }}
 " | nc -U /home/rasse/vm/qmp-sock
+sleep 1
 done
 clear
 echo -n \(qemu\)\ 
