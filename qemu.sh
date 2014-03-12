4602fd7bc822ec66cbf2783429c17d47c453584c
Rasmus Eskola <fruitiex@gmail.com>
2014-03-12 03:57:29 +0200

    use qcow2 as disk format, cache=writeback

--- qemu.sh
+++ qemu.sh
@@ -5,14 +5,15 @@ QEMU_AUDIO_DRV=sdl /home/rasse/src/qemu/x86_64-softmmu/qemu-system-x86_64 -monit
 	-device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
 	-device pci-assign,host=00:1b.0 \
 	-device ahci,bus=pcie.0,id=ahci \
-	-device ich9-intel-hda,bus=pcie.0,addr=1b.0,id=sound0 \
-	-device hda-duplex,id=sound0-codec0,bus=sound0.0,cad=0 \
 	-device vfio-pci,host=01:00.0,bus=root.1,addr=00.0,multifunction=on,x-vga=on -vga none -nographic \
-	-drive file=/dev/sdb7,id=disk,format=raw -device ide-hd,bus=ahci.0,drive=disk \
+	-drive file=/home/rasse/vm/win8.qcow2,id=disk,format=qcow2,cache=writeback -device ide-hd,bus=ahci.0,drive=disk \
 	-usb \
 	-redir tcp:24800::24800 \
-	-qmp unix:/home/rasse/vm/qmp-sock,server \
+	-qmp unix:/home/rasse/vm/qmp-sock,server
 
+#-drive file=/dev/sdb7,id=disk,format=raw -device ide-hd,bus=ahci.0,drive=disk \
+#-device ich9-intel-hda,bus=pcie.0,addr=1b.0,id=sound0 \
+#-device hda-duplex,id=sound0-codec0,bus=sound0.0,cad=0 \
 #-drive file=/home/rasse/vm/win.iso,id=isocd -device ide-cd,bus=ahci.1,drive=isocd -boot menu=on \
 #-device pci-assign,host=00:1a.0 \
 #-device vfio-pci,host=01:00.1,bus=root.1,addr=00.1 \
