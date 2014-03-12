4e1025d1c15597a4b1a409871205d1690b0da280
Rasmus Eskola <fruitiex@gmail.com>
2014-03-12 06:04:59 +0200

    separate hdd image for games

--- qemu.sh
+++ qemu.sh
@@ -1,19 +1,19 @@
-#QEMU_PA_SAMPLES=512 QEMU_AUDIO_DRV=pa /home/rasse/src/qemu/x86_64-softmmu/qemu-system-x86_64 -monitor stdio -enable-kvm -M q35 -m 6000 -mem-path /dev/hugepages -cpu host \
-QEMU_AUDIO_DRV=sdl /home/rasse/src/qemu/x86_64-softmmu/qemu-system-x86_64 -monitor stdio -enable-kvm -M q35 -m 6000 -mem-path /dev/hugepages -cpu host \
+QEMU_PA_SAMPLES=1024 QEMU_AUDIO_DRV=pa /home/rasse/src/qemu/x86_64-softmmu/qemu-system-x86_64 -monitor stdio -enable-kvm -M q35 -m 6000 -mem-path /dev/hugepages -cpu host \
 	-smp 4,sockets=1,cores=4,threads=1 \
 	-bios /home/rasse/src/seabios/out/bios.bin \
 	-device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
 	-device pci-assign,host=00:1b.0 \
 	-device ahci,bus=pcie.0,id=ahci \
-	-device vfio-pci,host=01:00.0,bus=root.1,addr=00.0,multifunction=on,x-vga=on -vga none -nographic \
 	-drive file=/home/rasse/vm/win8.qcow2,id=disk,format=qcow2,cache=writeback -device ide-hd,bus=ahci.0,drive=disk \
+	-drive file=/home/rasse/vm/win8_game.qcow2,id=game_disk,format=qcow2,cache=writeback -device ide-hd,bus=ahci.1,drive=game_disk \
+	-drive file=/dev/sdb,id=data_disk,format=raw,cache=writeback -device ide-hd,bus=ahci.2,drive=data_disk \
+	-device vfio-pci,host=01:00.0,bus=root.1,addr=00.0,multifunction=on,x-vga=on -vga none -nographic \
 	-usb \
 	-redir tcp:24800::24800 \
 	-qmp unix:/home/rasse/vm/qmp-sock,server
 
-#-drive file=/dev/sdb7,id=disk,format=raw -device ide-hd,bus=ahci.0,drive=disk \
+#-drive file=/home/rasse/vm/win.iso,id=isocd -device ide-cd,bus=ahci.3,drive=isocd -boot menu=on \
 #-device ich9-intel-hda,bus=pcie.0,addr=1b.0,id=sound0 \
 #-device hda-duplex,id=sound0-codec0,bus=sound0.0,cad=0 \
-#-drive file=/home/rasse/vm/win.iso,id=isocd -device ide-cd,bus=ahci.1,drive=isocd -boot menu=on \
 #-device pci-assign,host=00:1a.0 \
 #-device vfio-pci,host=01:00.1,bus=root.1,addr=00.1 \
