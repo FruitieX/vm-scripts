db0c0621bde82bc4c1e71eca5f82c680db9fdedb
Rasmus Eskola <fruitiex@gmail.com>
2014-07-01 16:56:12 +0300

    only select one ip

--- scripts/guest/vm_shutdown_guest.sh
+++ scripts/guest/vm_shutdown_guest.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 HOST_MAC_ADDR="bc-5f-f4-f7-c9-30"
-HOST=$(arp -a | grep $HOST_MAC_ADDR | awk '{ print $1; }')
+HOST=$(arp -a | grep $HOST_MAC_ADDR | head -n1 | awk '{ print $1; }')
 USER=rasse
 COMMAND="bash ~/vm/scripts/vm_shutdown_host.sh"
 until ssh $USER@$HOST $COMMAND
 do
-	HOST=$(arp -a | grep $HOST_MAC_ADDR | awk '{ print $1; }')
+	HOST=$(arp -a | grep $HOST_MAC_ADDR | head -n1 | awk '{ print $1; }')
 	echo "Retrying..."
 	sleep 1
 done
