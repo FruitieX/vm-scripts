06c69207150b0ef40c7c599835283331876ef38e
Rasmus Eskola <fruitiex@gmail.com>
2014-07-01 16:33:46 +0300

    new attempt at finding IP of host machine

--- scripts/guest/vm_shutdown_guest.sh
+++ scripts/guest/vm_shutdown_guest.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-HOST=$(route print -4 | egrep "^ +0.0.0.0 +0.0.0.0 +" | awk -F' ' '{print $3}')
+HOST_MAC_ADDR="bc-5f-f4-f7-c9-30"
+HOST=$(arp -a | grep $HOST_MAC_ADDR | awk '{ print $1; }')
 USER=rasse
 COMMAND="bash ~/vm/scripts/vm_shutdown_host.sh"
 until ssh $USER@$HOST $COMMAND
 do
-	HOST=$(route print -4 | egrep "^ +0.0.0.0 +0.0.0.0 +" | awk -F' ' '{print $3}')
+	HOST=$(arp -a | grep $HOST_MAC_ADDR | awk '{ print $1; }')
 	echo "Retrying..."
 	sleep 1
 done
