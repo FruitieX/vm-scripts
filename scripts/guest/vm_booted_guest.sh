05cf79000bf71420b87f85045ede83300425e340
Rasmus Eskola <fruitiex@gmail.com>
2014-07-05 16:04:17 +0300

    no strict host key checking when shelling to host

--- scripts/guest/vm_booted_guest.sh
+++ scripts/guest/vm_booted_guest.sh
@@ -4,7 +4,7 @@ HOST_MAC_ADDR="bc-5f-f4-f7-c9-30"
 HOST=$(arp -a | grep $HOST_MAC_ADDR | head -n1 | awk '{ print $1; }')
 USER=rasse
 COMMAND="bash ~/vm/scripts/vm_booted_host.sh"
-until ssh $USER@$HOST $COMMAND
+until ssh -oStrictHostKeyChecking=no $USER@$HOST $COMMAND
 do
 	HOST=$(arp -a | grep $HOST_MAC_ADDR | head -n1 | awk '{ print $1; }')
 	echo "Retrying..."
