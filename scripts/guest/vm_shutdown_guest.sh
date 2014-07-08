b78449d32f10a221eda004a7a3c4aa9c40caac78
Rasmus Eskola <fruitiex@gmail.com>
2014-07-08 17:48:59 +0300

    do both bridged networking and user mode networking

--- scripts/guest/vm_shutdown_guest.sh
+++ scripts/guest/vm_shutdown_guest.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-HOST_MAC_ADDR="bc-5f-f4-f7-c9-30"
-HOST=$(arp -a | grep $HOST_MAC_ADDR | head -n1 | awk '{ print $1; }')
+HOST="10.0.2.2"
 USER=rasse
 COMMAND="bash ~/vm/scripts/vm_shutdown_host.sh"
 until ssh -oStrictHostKeyChecking=no $USER@$HOST $COMMAND
 do
-	HOST=$(arp -a | grep $HOST_MAC_ADDR | head -n1 | awk '{ print $1; }')
 	echo "Retrying..."
 	sleep 1
 done
--- scripts/qemu.sh
+++ scripts/qemu.sh
@@ -1,23 +1,27 @@
-/home/rasse/src/qemu/x86_64-softmmu/qemu-system-x86_64 -monitor stdio -enable-kvm -M q35 -m 6000 -cpu host,hv-time,hv_relaxed,hv_vapic,hv_spinlocks=0x1000 \
+/home/rasse/src/qemu/x86_64-softmmu/qemu-system-x86_64 \
+	-monitor stdio -enable-kvm -M q35 -m 6000 \
+	-cpu host,hv-time,hv_relaxed,hv_vapic,hv_spinlocks=0x1000 \
 	-smp 4,sockets=1,cores=4,threads=1 \
 	-bios /home/rasse/src/seabios/out/bios.bin \
 	-device ioh3420,bus=pcie.0,addr=1c.0,multifunction=on,port=1,chassis=1,id=root.1 \
 	-device ahci,bus=pcie.0,id=ahci \
-	-drive file=/btrfs/vm/win8.qcow2,id=disk,format=qcow2 -device ide-hd,bus=ahci.0,drive=disk \
-	-drive file=/btrfs/vm/game.qcow2,id=game_disk,format=qcow2 -device ide-hd,bus=ahci.1,drive=game_disk \
+	-drive file=/btrfs/vm/win8.raw,id=disk,format=raw -device ide-hd,bus=ahci.0,drive=disk \
+	-drive file=/btrfs/vm/game.raw,id=game_disk,format=raw -device ide-hd,bus=ahci.1,drive=game_disk \
 	-drive file=/dev/sdb,id=data_disk,format=raw -device ide-hd,bus=ahci.2,drive=data_disk \
-	-redir tcp:24800::24800 \
-	-redir tcp:9999::9999 \
-	-redir tcp:3389::3389 \
 	-qmp unix:/home/rasse/vm/qmp-sock,server \
 	-device vfio-pci,host=01:00.0,bus=root.1,addr=00.0,multifunction=on,x-vga=on -vga none -nographic \
 	-device vfio-pci,host=00:1b.0,bus=root.1,addr=00.1 \
 	-usb \
 	-device nec-usb-xhci,id=xhci \
 	-device ich9-usb-uhci1,id=uhci \
-	-netdev bridge,id=hn0,helper=/home/rasse/src/qemu/qemu-bridge-helper \
-	-device virtio-net-pci,netdev=hn0,id=nic1
+	-netdev bridge,id=bridge,helper=/home/rasse/src/qemu/qemu-bridge-helper \
+	-device virtio-net-pci,netdev=bridge,id=pubnet,mac=13:37:12:34:12:34 \
+    -netdev user,id=user \
+    -device virtio-net-pci,netdev=user,id=privnet,mac=13:38:12:34:12:34 \
+	-redir tcp:24800::24800
 
+	#-redir tcp:9999::9999 \
+	#-redir tcp:3389::3389 \
 	#-device nec-usb-xhci,id=xhci \
 	#/home/rasse/src/qemu-latest/x86_64-softmmu/qemu-system-x86_64 -monitor stdio -enable-kvm -M q35 -m 6000 -cpu Haswell,hv-time \
 	#-mem-path /dev/hugepages
