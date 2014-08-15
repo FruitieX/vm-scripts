65f2731d7fd555c4b2cd12ff6c79f30dcae43503
Rasmus Eskola <fruitiex@gmail.com>
2014-08-15 20:02:27 +0300

    un-passthrough input usb devices is enough at shutdown

--- scripts/host/vm_shutdown.sh
+++ scripts/host/vm_shutdown.sh
@@ -5,7 +5,7 @@
 
 export DISPLAY=:0
 
-~/vm/scripts/usb-passthrough.sh del all
+~/vm/scripts/usb-passthrough.sh del input
 #sudo usb-reset.sh				# make sure you have sudo rights to this without password
 xrandr --output HDMI1 --mode 1920x1080 --rate 75 --rotate left --pos 0x0
 xrandr --output HDMI2 --mode 1920x1080 --rate 75 --rotate normal --pos 1080x475
