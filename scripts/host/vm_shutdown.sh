4de3b5bf65dcc802e6aff3c10e31272e5ad1628d
Rasmus Eskola <fruitiex@gmail.com>
2014-08-31 05:49:43 +0300

    set wallpaper properly at vm shutdown

--- scripts/host/vm_shutdown.sh
+++ scripts/host/vm_shutdown.sh
@@ -10,7 +10,7 @@ export DISPLAY=:0
 xrandr --output HDMI1 --mode 1920x1080 --rate 75 --rotate left --pos 0x0
 xrandr --output HDMI2 --mode 1920x1080 --rate 75 --rotate normal --pos 1080x475
 xrandr --output VGA1 --mode 1920x1080 --rate 75 --rotate right --pos 3000x0
-~/bin/wallpaper.sh -o
+feh --bg-tile ~/dotfiles/.compiled/bg.png &
 herbstclient reload
 xmodmap /home/rasse/.Xmodmap	# reset keyboard layout to normal
 xset m 1 1						# reset mouse accel settings to normal
