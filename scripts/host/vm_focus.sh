5193154033bcfe2848078fc879ad36475e6c1b55
Rasmus Eskola <fruitiex@gmail.com>
2014-08-31 05:49:32 +0300

    make sure vm is running before attempting to focus it

--- scripts/host/vm_focus.sh
+++ scripts/host/vm_focus.sh
@@ -3,6 +3,13 @@
 # This script is ran to "focus" the VM, that is, pass through any input USB
 # devices and give up the display from the host to the guest
 
+systemctl --state running | grep qemu@win8.service > /dev/null
+
+if [[ $? != 0 ]]; then
+    echo "VM not running?"
+    exit
+fi
+
 export DISPLAY=:0
 # turn off host display input
 xrandr --output HDMI2 --off
